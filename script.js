2e3f8c6b0f21bdc4b2766d065cdede26fbea7ada
Juan Balbuena <juanbalb48@gmail.com>
2024-04-21 17:06:42 -0500

    finished weekend salary calculator. Had to rearrange some things that were not working.

--- script.js
+++ script.js
@@ -13,38 +13,69 @@ function addData(event) {
     let annualSalary = 
         document.querySelector("#annual-salary").value;
 
+
+    document.querySelector('#first-name').value = '';
+    document.querySelector('#last-name').value = '';
+    document.querySelector('#id-number').value = '';
+    document.querySelector('#title').value = '';
+    document.querySelector('#annual-salary').value = '';
+
     totalSalary += Number(annualSalary)
     
-    let monthlyTotal = document.querySelector('monthly-total') ;
-    monthlyTotal.innerHTML = `Combined Monthly Salary: ${totalSalary/12}`
-       
-    
-    let table = document.querySelector("#output-table");
-    let newRow = table.insertRow(table.rows.length);
-    
-    newRow.insertCell(0).innerHTML = firstName;
-    newRow.insertCell(1).innerHTML = lastName;
-    newRow.insertCell(2).innerHTML = idNumber;
-    newRow.insertCell(3).innerHTML = title;
-    newRow.insertCell(4).innerHTML = annualSalary;
-    newRow.insertCell(5).innerHTML =
-        '<button onclick="deleteData(this)">Delete</button>';
-    
-        clearInputs();
+    let monthlyTotal = document.querySelector('#monthly-total');
+    monthlyTotal.innerHTML = `
+    Total Monthly Salary: ${totalSalary/12}`
+    changeColor()
+
+    let container = document.querySelector('#container');
+        container.innerHTML += `
+        <div> 
+         ${firstName}, ${lastName}, ${idNumber}, ${title}, ${annualSalary}
+    <button onClick= "removeDiv(event)">
+            Delete
+        </button>
+    </div>
+        `;
 }
 
-console.log(addData);
+function changeColor(){
+    console.log('changeColor',totalSalary/12);
+       if (totalSalary/12 >= 20000) {
+           let monthlyTotal = document.querySelector('#monthly-total');
+           monthlyTotal.style.backgroundColor = 'red';
+           console.log('changeColorToRed');
+      }
+   }
 
-
-
-function deleteData(button) {
-    let row = button.parentNode.parentNode;
-    row.parentNode.removeChild(row);
+function removeDiv(event) {
+    console.log('remove div', event.target);
+    event.target.parentElement.remove();
 }
-function clearInputs() {
-    document.querySelector("#first-name").value = "";
-    document.querySelector("#last-name").value = "";
-    document.querySelector("#id-number").value = "";
-    document.querySelector("#title").value = "";
-    document.querySelector("#annual-salary").value = "";
-}
+//     let table = document.querySelector("#output-table");
+//     let newRow = table.insertRow(table.rows.length);
+    
+//     newRow.insertCell(0).innerHTML = firstName;
+//     newRow.insertCell(1).innerHTML = lastName;
+//     newRow.insertCell(2).innerHTML = idNumber;
+//     newRow.insertCell(3).innerHTML = title;
+//     newRow.insertCell(4).innerHTML = annualSalary;
+//     newRow.insertCell(5).innerHTML =
+//         '<button onclick="deleteData(this)">Delete</button>';
+    
+//         clearInputs();
+// }
+
+// console.log(addData);
+
+
+
+// function deleteData(button) {
+//     let row = button.parentNode.parentNode;
+//     row.parentNode.removeChild(row);
+// }
+// function clearInputs() {
+//     document.querySelector("#first-name").value = "";
+//     document.querySelector("#last-name").value = "";
+//     document.querySelector("#id-number").value = "";
+//     document.querySelector("#title").value = "";
+//     document.querySelector("#annual-salary").value = "";
